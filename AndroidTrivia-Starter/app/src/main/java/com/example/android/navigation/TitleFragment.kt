364822457bd15fad2409d8ffb486568c0afe38de
s09172 <ono_ayumu@cyberagent.co.jp>
2020-07-14 15:31:49 +0900

    3.1 - 3.3 navigation

---
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,47 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      saveInstanceState: Bundle?
+  ): View? {
+
+    val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+        inflater,
+        R.layout.fragment_title,
+        container,
+        false)
+
+    binding.playButton.setOnClickListener { view : View ->
+      view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+    }
+    setHasOptionsMenu(true)
+    return binding.root
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+    super.onCreateOptionsMenu(menu, inflater)
+    inflater?.inflate(R.menu.option_menu, menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    return NavigationUI.onNavDestinationSelected(item!!,
+    view!!.findNavController())
+        ||super.onOptionsItemSelected(item)
+  }
+}
