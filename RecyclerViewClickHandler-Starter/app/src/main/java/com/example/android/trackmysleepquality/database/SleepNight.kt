b91735759ea6db423b1ddf5452212c001db9617a
s09172 <ono_ayumu@cyberagent.co.jp>
2020-07-21 15:25:29 +0900

    7.5 finish

--- RecyclerViewClickHandler-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ RecyclerViewClickHandler-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -16,23 +16,27 @@
 
 package com.example.android.trackmysleepquality.database
 
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.DiffUtil
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.android.trackmysleepquality.sleeptracker.DataItem
 
 /**
  * Represents one night's sleep through start, end times, and the sleep quality.
  */
 @Entity(tableName = "daily_sleep_quality_table")
 data class SleepNight(
-        @PrimaryKey(autoGenerate = true)
-        var nightId: Long = 0L,
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
 
-        @ColumnInfo(name = "start_time_milli")
-        val startTimeMilli: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
 
-        @ColumnInfo(name = "end_time_milli")
-        var endTimeMilli: Long = startTimeMilli,
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
 
-        @ColumnInfo(name = "quality_rating")
-        var sleepQuality: Int = -1)
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality: Int = -1
+)
